35d7ef7d1991b2a5bdfe21b4b44a73eea7a94836
bvasan <bsrinivasan.pdy@gmail.com>
2018-07-11 00:29:11 -0500

    initial commit

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const express = require('express');
+
+var app = express();
+
+const publicPath = path.join(__dirname,'../public');
+console.log(publicPath);
+
+app.use(express.static(publicPath));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server up and running on port ${PORT}`);
+});
