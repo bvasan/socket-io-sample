92465aec7f9853a417885d15f8e4b3531f8fcf2b
bvasan <bsrinivasan.pdy@gmail.com>
2018-07-11 13:34:24 -0500

    Add chat html form/jquery

--- server/server.js
+++ server/server.js
@@ -20,14 +20,10 @@ io.on('connection',(socket) => {
 
   socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
-  socket.on('createMessage', (message) => {
+  socket.on('createMessage', (message, callback) => {
     console.log('createMessage ', message);
     io.emit('newMessage', generateMessage(message.from, message.text));
-    // socket.broadcast.emit('newMessage', {
-    //   from: message.from,
-    //   text: message.text,
-    //   createdAt: new Date().getTime()
-    // });
+    callback('ACK from the server');
   });
 
   socket.on('disconnect', () => {
